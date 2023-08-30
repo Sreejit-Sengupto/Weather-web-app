42778a84f7a28d59cd991d4e0021a3ddada46c6c
Sreejit <sreesen2003@gmail.com>
2023-08-30 14:26:45 +0530

    Adds netlify serverless function

--- netlify/functions/fetchAPI/fetchAPI.js
+++ netlify/functions/fetchAPI/fetchAPI.js
@@ -1,10 +1,11 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    // const subject = event.queryStringParameters.name || 'World'
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${event.latitude}&lon=${event.longitude}&appid=${process.env.API_KEY}`)
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ reply: response.data }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
